1be6f6843ce25b1a30842a3b33208f5ebff6a4d7
UhuruV <uhurualary@gmail.com>
2020-08-18 09:17:09 +0300

    Added a login form

--- src/login.js
+++ src/login.js
@@ -5,6 +5,7 @@ class Login extends React.Component{
     constructor(props) {
         super();
         this.state = {
+            title:"Log In",
             firstname: '',
             lastame: '',
             email: '',
@@ -15,7 +16,15 @@ class Login extends React.Component{
     render() {
         return (
             <div className="login">
-                
+                <title>{this.title}</title>
+                <h1>Welcome Back!</h1>
+                <input type="text" hintText="First Name" >{this.state.firstname}</input>
+                <input type="text" hintText="Last Name" >{this.state.firstname}</input>
+                <input type="text" hintText="you@gmail.com" >{this.state.firstname}</input>
+                <input type="password" hintText="Password" >{this.state.firstname}</input>
+                <br />
+
+                <button>{this.state.button}</button>
           </div>  
         );
     }
