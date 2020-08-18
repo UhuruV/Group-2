b66e668221712c888f4303a839a2ac6c4fdd7ba3
UhuruV <uhurualary@gmail.com>
2020-08-18 09:06:33 +0300

    our first commit

---
+++ src/login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class Login extends React.Component{
+    constructor(props) {
+        super();
+        this.state = {
+            firstname: '',
+            lastame: '',
+            email: '',
+            password: '',
+            submit: "Submit"
+        };
+    }
+    render() {
+        return (
+            <div className="login">
+                
+          </div>  
+        );
+    }
+}
