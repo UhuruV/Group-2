58d00b322927508df0617e85846a7e4f988c375d
UhuruV <uhurualary@gmail.com>
2020-08-23 14:37:04 +0300

    Reused components,learnt how to fetch API and how to use props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { observer } from 'mobx-react';
+import UserStore from './UserStores';
+import LoginForm from './login';
+import SubmitButton from './submitButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  /*
+  API calls
+  */
+  async componentDidMount() {
+    try {
+      let res = await fetch('/isLoggedIn', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content_Type':'application'
+        }
+      });
+      let result = await res.json();
+      if (result && result.success) {
+        UserStore.loading = false;
+        UserStore.isLoggedIn = true;
+        UserStore.username = result.username;
+      }
+      else {
+        UserStore.loading = false;
+        UserStore.isLoggedIn = false;
+      }
+    }
+    catch (e) {
+      UserStore.loading = false;
+      UserStore.isLoggedIn = false;
+      
+    }
+  }
+  async doLogOut() {
+    try {
+      let res = await fetch('/logout', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content_Type': 'application'
+        }
+      });
+      let result = await res.json();
+      if (result && result.success) {
+        UserStore.isLoggedIn = false;
+        UserStore.username = '';
+      }
+    }
+    catch (e) {
+      console.log(e);
+      
+    }
+  }
+
+  render() {
+    if (UserStore.loading) {
+      return (
+        <div className='app'>
+          <div className='container'>
+            Loading,please wait...
+          </div>
+        </div>
+      );
+    }
+    else {
+      if (UserStore.isLoggedIn) {
+        return (
+          <div className='app'>
+            <div className='container'>
+              Welcome {UserStore.username}
+              <SubmitButton
+                text={'Log Out'}
+                disabled={false}
+                onClick={ ()=> this.doLogOut}
+                />
+          </div>
+          </div>
+        );
+      }
+    }
+    return (
+      <div className="app">
+        <div className='container'>
+          <LoginForm />
+        </div>
+      
+      </div>
+    );
+  }
 }
 
-export default App;
+export default observer(App);
--- src/login.js
+++ src/login.js
@@ -1,31 +1,106 @@
 import React from 'react';
+import InputField from './Components/inputField';
+import SubmitButton from './submitButton';
+import UserStore from './UserStores';
 
-
-class Login extends React.Component{
+class LoginForm extends React.Component{
     constructor(props) {
-        super();
+        super(props);
+
         this.state = {
-            title:"Log In",
-            firstname: '',
-            lastame: '',
-            email: '',
+            username: ' ',
             password: '',
-            submit: "Submit"
-        };
+            buttonDisabled:false
+        }
     }
+    setInputValue(property, val) {
+        val = val.Trim() //trim it to get rid of spaces
+        if (val.length > 12) {
+            return;  //will return false
+        }
+        this.setState({
+            [property]: val
+        });
+    }
+    //Reset the form so the user can retype
+    resetForm(){
+        this.setState({
+            username: ' ',
+            password: '',
+            buttonDisabled:false
+        })
+    }
+    //Called when the user logs in
+    async doLogIn() {
+        if (!this.state.username) {
+            return; 
+        }
+        if (!this.state.password) {
+            return;
+        }
+        //Ensure that the user cannot double click the submit button
+        this.setState({
+            buttonDisabled:true,
+        })
+        //Trying to access API point
+        try {
+          
+            let res = await fetch('/login', {
+                method: 'post',
+                headers: {
+                    'Accept': 'applocation/json',
+                    'Content-Type': 'application/json'
+                },
+                //Sends username and password to the API  to  check in the db if they exist then create a session
+                body: JSON.stringify({
+                    username: this.state.username,
+                    password: this.state.password
+                })
+            });
+            //Successfuly logged in
+            let result = await res.json();
+            if (result && result.success) {
+                UserStore.isLoggedIn = true;
+                UserStore.username = result.username;
+            }
+            else if (result && result.success === false) {
+                this.resetForm();
+                alert(result.msg);
+            }
+            
+        }
+        catch (e) {
+            console.log(e);
+            this.resetForm();
+        }
+    }
+    
+    
+        
     render() {
         return (
             <div className="login">
-                <title>{this.title}</title>
-                <h1>Welcome Back!</h1>
-                <input type="text" hintText="First Name" >{this.state.firstname}</input>
-                <input type="text" hintText="Last Name" >{this.state.firstname}</input>
-                <input type="text" hintText="you@gmail.com" >{this.state.firstname}</input>
-                <input type="password" hintText="Password" >{this.state.firstname}</input>
-                <br />
-
-                <button>{this.state.button}</button>
+                Log In
+                <InputField
+                    type='text'
+                    placeholder='Username'
+                    value={this.state.username ? this.state.username : " "}
+                    onChange={ (val)=>this.setInputValue('username',val) }
+                />  
+                <InputField
+                    type='password'
+                    placeholder='Enter Password'
+                    value={this.state.password ? this.state.password : " "}
+                    onChange={ (val)=>this.setInputValue('password',val) }
+                />
+                <SubmitButton
+                    text='Log In'
+                    disabled={this.state.buttonDisabled}
+                    onClick={ () => this.doLogIn()}
+                    />
           </div>  
         );
     }
 }
+
+export default LoginForm;
